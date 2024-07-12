695ab6027d81ca5d0875f162a13499b77ee99da5
Pitrified <nobilipietro@gmail.com>
2024-07-12 22:15:58 +0200

    Add guess[es] builder to game board

--- guess.gd
+++ guess.gd
@@ -1,12 +1,18 @@
 class_name Guess
 extends HBoxContainer
 
-@export var guess_letter_scene: PackedScene = preload ("res://guess_letter.tscn")
+const guess_letter_scene: PackedScene = preload ("res://guess_letter.tscn")
 
 # hold the guess letters in a list
 var guess_letters = []
+var guess_len: int
 
-func build_guess(guess_len: int):
+static func new_guess(guess_len_: int):
+	var guess = Guess.new()
+	guess.guess_len = guess_len_
+	return guess
+
+func build_guess():
 	for i in range(0, guess_len):
 		var guess_letter = guess_letter_scene.instantiate()
 		add_child(guess_letter)
@@ -14,4 +20,4 @@ func build_guess(guess_len: int):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	build_guess(5)
+	build_guess()
--- guesses.gd
+++ guesses.gd
@@ -1,6 +1,12 @@
 class_name Guesses
 extends VBoxContainer
 
+func build_guesses():
+	var guesses_num = 3
+	for i in range(0, guesses_num):
+		var guess = Guess.new_guess(5)
+		add_child(guess)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	build_guesses()
