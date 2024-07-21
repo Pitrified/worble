e2b1456c8e0041c28c9460bb34c40643316109d1
Pitrified <nobilipietro@gmail.com>
2024-07-21 12:22:35 +0200

    Draw the keyboard directly for the right size

--- keyboard.gd
+++ keyboard.gd
@@ -8,7 +8,6 @@ const KB_LETTERS = [
 	['.', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'enter']
 ]
 
-var has_letters_board = false
 var letters_board = []
 var KB_LETTER_MARGIN = 10
 
@@ -16,41 +15,68 @@ signal keyboard_input(content: String)
 
 func build_keyboard():
 	print('building keyboard')
-	for row in KB_LETTERS:
-		var letters_row = []
-		for letter in row:
-			var kb_btn = KbButton.new_kb_button(letter, _on_kb_button_kb_button_down)
-			add_child(kb_btn)
-			letters_row.append(kb_btn)
-		letters_board.append(letters_row)
-	has_letters_board = true
- 
-func update_keyboard():
-	print('start updating keyboard')
-	if not has_letters_board:
-		build_keyboard()
-	print('do    updating keyboard')
-	var position_ = Vector2(0, 50)
-	print('position_', position_)
+
+	# remove the previous keyboard
+	remove_keyboard()
+	print('keyboard removed, rebuilding')
+
+	# compute the width available
 	var cur_size = get_size()
 	var width_available = cur_size.x
-	for row in letters_board:
+	var position_ = Vector2(0, 0)
+	var letter_height = 70
+
+	for row in KB_LETTERS:
+		var letters_row = []
+
+		# based on the number of letters in the row, compute the width of each letter
 		var num_letters = row.size()
 		var width_available_for_letters = width_available - (num_letters - 1) * KB_LETTER_MARGIN
 		var letter_width = width_available_for_letters / num_letters
-		for letter in row:
-			letter.set_size(Vector2(letter_width, 50))
-			letter.set_position(position_)
-			position_.x += letter_width + KB_LETTER_MARGIN
-		position_.y += 50 + KB_LETTER_MARGIN
-		position_.x = 0
-	# letters_board[0][0].set_size(Vector2(50, 50))
-	# letters_board[0][0].set_position(Vector2(100, 50))
 
-# func remove_keyboard():
-# 	for letter in letters_ls:
-# 		# remove_child(letter) # is this needed?
-# 		letter.queue_free()
+		for letter in row:
+			# create a button for each letter
+			var kb_btn = KbButton.new_kb_button(letter, _on_kb_button_kb_button_down)
+			add_child(kb_btn)
+
+			# set the size and position of the button
+			kb_btn.set_size(Vector2(letter_width, letter_height))
+			kb_btn.set_position(position_)
+			# move to the next position
+			position_.x += letter_width + KB_LETTER_MARGIN
+
+			letters_row.append(kb_btn)
+
+		# move to the next row
+		position_.y += letter_height + KB_LETTER_MARGIN
+		position_.x = 0
+		letters_board.append(letters_row)
+ 
+# func update_keyboard():
+# 	print('start updating keyboard')
+# 	var position_ = Vector2(0, 50)
+# 	print('position_', position_)
+# 	var cur_size = get_size()
+# 	var width_available = cur_size.x
+# 	for row in letters_board:
+# 		var num_letters = row.size()
+# 		var width_available_for_letters = width_available - (num_letters - 1) * KB_LETTER_MARGIN
+# 		var letter_width = width_available_for_letters / num_letters
+# 		for letter in row:
+# 			letter.set_size(Vector2(letter_width, 50))
+# 			letter.set_position(position_)
+# 			position_.x += letter_width + KB_LETTER_MARGIN
+# 		position_.y += 50 + KB_LETTER_MARGIN
+# 		position_.x = 0
+
+func remove_keyboard():
+	print('removing keyboard')
+	for row in letters_board:
+		print('removing row')
+		for letter in row:
+			print('removing letter')
+			# remove_child(letter) # is this needed?
+			letter.queue_free()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -80,4 +106,4 @@ func _notification(what):
 			pass # Control changed size; check new size
 			# with get_size().
 			print('board: resized', get_size())
-			update_keyboard()
+			# build_keyboard()
